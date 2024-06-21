8c579e4b3b2b84bd82bc4b255c51e2e5a09a9acb
rhrits <hritik.hritiksingh@gmail.com>
2024-06-22 02:23:15 +0530

    Stage 3 : site header

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+    name: "JollyBlog",
+    url: "https://example.com",
+    description: "Nextjs 14 blog using velite, tailwind and shadcn",
+    author: "JollyCoding",
+    links: {
+      twitter: "https://x.com/rhrits",
+      github: "https://github.com/rhrits",
+      personalSite: "https://rhrits.me",
+    },
+  };
+  
+  export type SiteConfig = typeof siteConfig;
