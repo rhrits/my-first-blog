3a1c9d6a30f04b7b9c759a3df3441ae4b5146214
rhrits <hritik.hritiksingh@gmail.com>
2024-06-23 09:30:53 +0530

    icon removal

--- components/mobile-nav.tsx
+++ components/mobile-nav.tsx
@@ -26,7 +26,7 @@ export function MobileNav() {
           href="/"
           className="flex items-center"
         >
-          <Icons.logo className="mr-2 h-4 w-4" />
+          
           <span className="font-bold">{siteConfig.name}</span>
         </MobileLink>
         <div className="flex flex-col gap-3 mt-3">
